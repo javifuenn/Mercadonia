03cd82a51aa590d6023cef0813fd8c36e142bd27
sergisanchezprieto <33113666+sergisanchezprieto@users.noreply.github.com>
2021-03-24 17:48:38 +0100

    ventana Login

---
+++ src/main/java/gui/VentanaLogin.java
@@ -0,0 +1,75 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class VentanaLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textnombre_usuario;
+	private JTextField textContraseña;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaLogin frame = new VentanaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 605, 390);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblnombre_usuario = new JLabel("Nombre de usuario:");
+		lblnombre_usuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblnombre_usuario.setBounds(55, 46, 140, 24);
+		contentPane.add(lblnombre_usuario);
+		
+		JLabel lblcontraseña = new JLabel("Contraseña:");
+		lblcontraseña.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblcontraseña.setBounds(55, 128, 140, 24);
+		contentPane.add(lblcontraseña);
+		
+		textnombre_usuario = new JTextField();
+		textnombre_usuario.setBounds(205, 48, 193, 24);
+		contentPane.add(textnombre_usuario);
+		textnombre_usuario.setColumns(10);
+		
+		textContraseña = new JTextField();
+		textContraseña.setColumns(10);
+		textContraseña.setBounds(205, 128, 193, 24);
+		contentPane.add(textContraseña);
+		
+		JButton btnRegistro = new JButton("Registrar");
+		btnRegistro.setBounds(106, 255, 89, 23);
+		contentPane.add(btnRegistro);
+		
+		JButton btnLogin = new JButton("Login\r\n");
+		btnLogin.setBounds(353, 255, 89, 23);
+		contentPane.add(btnLogin);
+	}
+}
--- src/main/java/util/PreparaDatos.java
+++ src/main/java/util/PreparaDatos.java
@@ -17,7 +17,6 @@ public class PreparaDatos {
 		try {
 			tx.begin();
 
-
 			tx.commit();
 		} finally {
 			if (tx.isActive()) {
