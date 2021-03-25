fe9bb07f86e08af5b7574c5c3f6a0c4a0ed88343
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2021-03-25 22:38:00 +0100

    Implementado Producto con preparacion de datos

---
+++ src/main/java/jdo/Producto.java
@@ -0,0 +1,60 @@
+package jdo;
+
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
+public class Producto {
+	
+	@PrimaryKey
+	private String codigo;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	
+	public Producto(String codigo, String nombre, String descripcion, double precio) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String toString() {
+		return "Producto: " + codigo + "\nNombre: " + nombre + " [ " + descripcion + " ]" + "\nPrecio: " + precio;
+	}
+}
--- src/main/java/util/PreparaDatos.java
+++ src/main/java/util/PreparaDatos.java
@@ -5,6 +5,8 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Transaction;
 
+import jdo.Producto;
+
 
 
 public class PreparaDatos {
@@ -16,7 +18,12 @@ public class PreparaDatos {
 		Transaction tx = pm.currentTransaction();
 		try {
 			tx.begin();
-
+			Producto prod1 = new Producto("1A", "Lechuga", "Muy sana", 2.4);
+			pm.makePersistent(prod1);
+			Producto prod2 = new Producto("2A", "Manzana", "Deliciosa", 3);
+			pm.makePersistent(prod2);
+			Producto prod3 = new Producto("3A", "Pan", "Recien horneado", 0.6);
+			pm.makePersistent(prod3);
 			tx.commit();
 		} finally {
 			if (tx.isActive()) {
