a2b32dbc2391ec73fa7b623b709d7afc1716fc00
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2021-04-30 15:57:16 +0200

    Eliminado bin

--- bin/src/main/resources/database.sql
+++
@@ -1,7 +0,0 @@
-DROP SCHEMA IF EXISTS productosDB;
-DROP USER IF EXISTS 'spq'@'localhost';
-
-CREATE SCHEMA productosDB;
-CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-
-GRANT ALL ON productosDB.* TO 'spq'@'localhost';
