1f0c14c1754421e51afc4d089d2b33cf2e0a4d26
unknown <sergisanchezprieto@opendeusto.es>
2021-05-18 17:46:11 +0200

    oxigen comentado de varias clases jdo

--- src/main/java/jdo/Cupon.java
+++ src/main/java/jdo/Cupon.java
@@ -2,6 +2,12 @@ package jdo;
 
 import javax.jdo.annotations.PersistenceCapable;
 
+
+/**
+ * Clase CUPON para un descuento
+ * @author Sergio
+ *
+ */
 @PersistenceCapable(detachable = "true")
 public class Cupon {
 	
@@ -9,34 +15,68 @@ public class Cupon {
 	private int porcentajeDescuento;
 	private String usuario;
 	
+	/**
+	 * Constructor vacio de la clase cupon
+	 */
 	public Cupon() {
 		
 	}
-	
+	/**
+	 * Constructor de la clase CUPON
+	 * @param textoCupon String con el texto del CUPON
+	 * @param porcentajeDescuento int con el porcentaje de descuento del CUPON
+	 * @param usuario String con el nombre del USUARIO del CUPON
+	 */
 	public Cupon(String textoCupon, int porcentajeDescuento, String usuario) {
 		this.textoCupon = textoCupon;
 		this.porcentajeDescuento = porcentajeDescuento;
 		this.usuario = usuario;
 	}
-
+	/**
+	 * Devuelve el texto del CUPON
+	 * @return String con el texto del CUPON
+	 */
 	public String getTextoCupon() {
 		return textoCupon;
 	}
+	/**
+	 * Devuelve el texto del CUPON
+	 * @param textoCupon String con el texto del CUPON
+	 */
 	public void setTextoCupon(String textoCupon) {
 		this.textoCupon = textoCupon;
 	}
+	/**
+	 * Devuelve el porcentaje de descuento del CUPON 
+	 * @return int con el porcentaje de descuento del CUPON
+	 */
 	public int getPorcentajeDescuento() {
 		return porcentajeDescuento;
 	}
+	/**
+	 * Permite cambiar el porcentaje de descuento del CUPON
+	 * @param porcentajeDescuento int con el nuevo porcentaje de descuento del CUPON
+	 */
 	public void setPorcentajeDescuento(int porcentajeDescuento) {
 		this.porcentajeDescuento = porcentajeDescuento;
 	}
+	/**
+	 * Devuelve el USUARIO dueño del CUPON
+	 * @return String del USUARIO dueño del CUPON
+	 */
 	public String getUsuario() {
 		return usuario;
 	}
+	/**
+	 * Permite cambair el USUARIO dueño del CUPON
+	 * @param usuario String del nuevo USUARIO dueño del CUPON
+	 */
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+	/**
+	 * toString de la clase CUPON
+	 */
 	@Override
 	public String toString() {
 		return "Cupon [textoCupon=" + textoCupon + ", porcentajeDescuento=" + porcentajeDescuento + ", usuario="
--- src/main/java/jdo/Paypal.java
+++ src/main/java/jdo/Paypal.java
@@ -5,6 +5,11 @@ import javax.jdo.annotations.InheritanceStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 
+/**
+ * Clase PAYPAL para el pago
+ * @author Sergio
+ *
+ */
 @PersistenceCapable(detachable = "true")
 @Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class Paypal {
@@ -12,32 +17,53 @@ public class Paypal {
 	private String correo;
 	private String contrasena;
 	
+	/**
+	 * Contrctor vacio de la clase PAYPAL
+	 */
 	public Paypal() {
 		
 	}
-
+	/**
+	 * Constructor de la clase PAYPAL
+	 * @param correo String del correo de PAYPAL
+	 * @param contrasena String de la contraseña de PAYPAL
+	 */
 	public Paypal(String correo, String contrasena) {
 		super();
 		this.correo = correo;
 		this.contrasena = contrasena;
 	}
-
+	/**
+	 * Devuelve el correo de PAYPAL
+	 * @return String del correo
+	 */
 	public String getCorreo() {
 		return correo;
 	}
-
+	/**
+	 * Permite cambiar el correo de PAYPAL
+	 * @param correo String del nuevo correo
+	 */
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
+	/**
+	 * Devuelve la contrasela de PAYPAL
+	 * @return String con la contraseña nueva
+	 */
 	public String getContrasena() {
 		return contrasena;
 	}
-
+	/**
+	 * Permite cambiar la contraseña de PAYPAL
+	 * @param contrasena String con la nueva contraseña
+	 */
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-
+	/**
+	 * toString de PAYPAL
+	 */
 	@Override
 	public String toString() {
 		return "Paypal [correo=" + correo + ", contrasena=" + contrasena + "]";
